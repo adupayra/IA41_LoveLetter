4524d77b8856c5cd053508335bdcd6673e63425d
adupayra <antoine.dupayrat@utbm.fr>
2020-10-26 23:09:44 +0100

    c est chaud mais ca avance

---
+++ src/singleton.py
@@ -0,0 +1,17 @@
+'''
+Created on 26 oct. 2020
+
+@author: Antoine
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
--- src/view/View.py
+++ src/view/View.py
@@ -6,74 +6,108 @@ Created on 26 oct. 2020
 '''
 import tkinter as tk
 
-from src.view.GameScene import *
-from src.view.MenuScene import *
-from src.view.EndGameScene import *
 
+def display_game_scene():
+    View._game_scene.tkraise()
 
-class View(object):
+def display_end_game_scene():
+    View._end_game_scene.tkraise()
+
+def display_menu_scene():
+    View._menu_scene.tkraise()
+    
+
+class View(tk.Tk):
     '''
     Classe qui gère la création de la fenêtre ainsi que les transitions entre les différentes scènes UI (entre le menu de 
     séléction, le jeu, et le menu de victoire
     '''
-    
-    #Unique instance de la classe View
-    _instance = None
-    _root = None
     _menu_scene = None
     _game_scene = None
     _end_game_scene = None
     
     def __init__(self):
+        #Création de la fenêtre
+        tk.Tk.__init__(self)
+        
+        #scale de la fenêtre en fonction de la taille de l'écran de l'utilisateur
+        width_value = self.winfo_screenwidth()
+        height_value = self.winfo_screenheight()
+        self.geometry("%dx%d+0+0" % (width_value, height_value))
+        
+        #Création des différentes scènes
+        View._menu_scene = MenuScene(self)
+        View._game_scene = GameScene(self)
+        View._end_game_scene = EndGameScene(self)
+        
+        for f in (View._menu_scene, View._game_scene, View._end_game_scene):
+            f.initbuttons()
+        display_menu_scene()
+        self._menu_scene.button.tkraise()
+        self.mainloop()
+            
+
+
+
+class MenuScene(tk.Frame):
+    '''
+    Classe responsable de l'affichage du menu
+    '''
+
+
+    def __init__(self, parent):
         '''
         Constructor
         '''
-    
-    
-    #Implémentation du pattern singleton afin d'avoir une unique instantiation de la classe
-    
-    def __new__(cls):
-        if(cls._instance is None):
-            cls._instance = super(View, cls).__new__(cls)
-            _menu_scene = MenuScene(cls._instance)
-            _game_scene = GameScene(cls._instance)
-        return cls._instance
-    
-    def initialization(self):
-        
-        #Initialition seulement si ça n'a pas déjà été fait
-        if(View._root is None):
-            #Création de la fenêtre et scale en fonction de la taille de l'écran de l'utilisateur
-            View._root = tk.Tk()
-            width_value = View._root.winfo_screenwidth()
-            height_value = View._root.winfo_screenheight()
-            View._root.geometry("%dx%d+0+0" % (width_value, height_value))
-            
-            #Création des différentes scènes
-            View._menu_scene = MenuScene(self._root)
-            View._game_scene = GameScene(self._root)
-            #self._end_game_scene = EndGameScene(self._root)
-            
-            View.display_game_scene()
-            View._root.mainloop()
-        
-    #Getter de l'unique instance de classe
-    @classmethod
-    def get_instance(cls):
-        return View()
-    
-    @classmethod
-    def display_game_scene(self):
-        View._game_scene.tkraise()
-    
-    @classmethod
-    def display_end_game_scene(self):
-        View._end_game_scene.tkraise()
-    
-    @classmethod
-    def display_menu_scene(self):
-        View._menu_scene.tkraise()
+        tk.Frame.__init__(self, parent, bg = 'green')
+        self.place(relwidth = 1, relheight = 1)
+        self.parent = parent
+        self.button = None
 
-test = View()
-View.get_instance().initialization()
+    def initbuttons(self):
+        self.button=tk.Button(self.parent, command = display_game_scene(),text = "bite")
+        self.button.pack()
+        self.button.tkraise()
+       
         
+
+        
+class GameScene(tk.Frame):
+    '''
+    Cette classe contient les éléments UI du jeu
+    '''
+
+
+    def __init__(self, parent):
+        '''
+        Constructor
+        '''
+        tk.Frame.__init__(self, parent, bg = 'blue')
+        self.place(relwidth = 1, relheight = 1)
+        self.parent= parent
+        
+        
+    def initbuttons(self):
+        self.button=tk.Button(self.parent, command = display_menu_scene(),text = "bite")
+        self.button.pack()
+
+
+class EndGameScene(tk.Frame):
+    '''
+    Classe s'occupant de l'affichage du menu de fin
+    '''
+
+
+    def __init__(self, parent):
+        '''
+        Constructor
+        '''
+        tk.Frame.__init__(self, parent, bg = 'cyan')
+        self.place(relwidth = 1, relheight = 1)
+        self.parent = parent
+        
+    def initbuttons(self):
+        self.button=tk.Button(self.parent, command = display_menu_scene(), text = "bite")
+        self.button.pack()
+        
+test = View()
